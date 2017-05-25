e2a90ab21eed316d1bf0bb85b8c42f54f9024b06
jilldela <jilldelosangeles@gmail.com>
2017-05-24 23:16:10 -0700

    Add ticket seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -316,5 +316,6 @@ ec20a = EventCategory.create(event_id: e20.id, category_id: classes.id)
 ec20b = EventCategory.create(event_id: e20.id, category_id: networking.id)
 
 Ticket.destroy_all
-t1 = Ticket.create(event_id: e5.id, user_id: guest.id)
-t2 = Ticket.create(event_id: e9.id, user_id: guest.id)
+t1 = Ticket.create(event_id: e20.id, user_id: jill.id)
+t2 = Ticket.create(event_id: e9.id, user_id: jill.id)
+t3 = Ticket.create(event_id: e14.id, user_id: jill.id)
